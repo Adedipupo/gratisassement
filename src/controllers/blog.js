bca3291c6cfa4de4edbfc34448b269efb57d0cf4
Benedict <jokanolaadedipupo@gmail.com>
2021-08-01 08:24:12 +0100

    blog post created

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -4,6 +4,14 @@ import BlogModel from "../models/blog.js";
 export const createBlog = asyncHandler(async (req, res) => {
     const { title, body, comments } = req.body;
   
+
+  const titleExists = await BlogModel.findOne({ title });
+
+  if (titleExists) {
+    res.status(400);
+    throw new Error("Title already exists");
+  }
+
     const blog = await BlogModel.create({
       title,
       body,
@@ -12,6 +20,7 @@ export const createBlog = asyncHandler(async (req, res) => {
     if (blog) {
       res.status(200).json({
         _id: blog._id,
+        title: blog.title,
         body: blog.body,
         comments: blog.comments,
       });
