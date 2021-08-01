f4414ee9343f0bcb67def947e8595ae9a9361072
Benedict <jokanolaadedipupo@gmail.com>
2021-08-01 13:02:05 +0100

    get all posts with pagination

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBlog,getAllBlogPost } from '../controllers/blog.js';
+import { createBlog,getAllBlogPost, getAllPosts } from '../controllers/blog.js';
 
 const router = express.Router();
 
 router.post('/create', createBlog)
 router.get('/allposts', getAllBlogPost)
+router.get('/posts', getAllPosts)
 
 export default router;
